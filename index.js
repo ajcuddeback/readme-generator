2cbcb741f514bca77ec3bb2e5122a2b63d03aa45
ajcudd <ajcuddeback@gmail.com>
2020-12-13 22:51:52 -0500

    added pseudo code

--- index.js
+++ index.js
@@ -1,10 +1,72 @@
 const inquirer = require('inquirer');
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // array of questions for user
-const questions = [
+// const questions = [
 
-];
+// ];
+
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Please enter some information about your project:',
+            validate: aboutInput => {
+                if (aboutInput) {
+                    return true;
+                } else {
+                    console.log('Please enter some information!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstall',
+            message: 'Does your project need special instructions on how to install?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Enter your installation instructions:',
+            when: ({ confirmInstall }) => {
+                if (confirmInstall) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please explain the usage of this project:',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+    ])
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -17,3 +79,12 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+
+// README must have title of project, description, table of contents, installation, usage, license, contributing, tests, and questions
+// project title projected at top where title is
+// license is a multiple choice option - badge of license type must be added to top of readme along with in license part
+// git hub user name must be added to questions sections with link to github profile
+// email address will also be in questions with instructions on how to reach out with any extra questions
+// table of contents works with links
